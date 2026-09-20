e767549191baaaddafc6557f7a475f19a6cf0748
xuqingxin001 <33855438+xuqingxin001@users.noreply.github.com>
2026-09-20 11:14:18 +0800

    Add files via upload

--- gamecheckin.py
+++ gamecheckin.py
@@ -226,7 +226,7 @@ class Genshin(GameCheckin):
         self.is_sign_api = f"{setting.web_api}/event/luna/hk4e/info?lang={setting.cn_game_lang}"
         self.sign_api = f"{setting.web_api}/event/luna/hk4e/sign"
         self.headers["Origin"] = "https://act.mihoyo.com"
-       # self.headers["x-rpc-signgame"] = "hk4e"
+        self.headers["x-rpc-signgame"] = "hk4e"
         self.init()
 
 
@@ -249,7 +249,7 @@ class ZZZ(GameCheckin):
         self.is_sign_api = f"{setting.web_api}/event/luna/zzz/info?lang={setting.cn_game_lang}"
         self.sign_api = f"{setting.web_api}/event/luna/zzz/sign"
         self.headers["Origin"] = "https://act.mihoyo.com"
-        self.headers["x-rpc-signgame"] = "zzz"
+        # self.headers["x-rpc-signgame"] = "zzz"
         self.init()
 
 
